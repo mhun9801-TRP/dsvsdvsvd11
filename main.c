1dc3a7860abc189e832c48569d1cb57520f07588
o_m.aeng <68192707+mhun9801-TRP@users.noreply.github.com>
2023-02-11 11:18:43 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+
+	float f;
+	scanf("%f",&f);
+	int num=0;
+	
+	
+	for (f; f>=1;f=f/2)
+	{
+		num++;
+		
+	}
+	printf("%d",num);
+
+	
+	
+	return 0;
+}
